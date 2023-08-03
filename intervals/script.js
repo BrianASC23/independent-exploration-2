4e77f44f8ad10772e641189c2c1e9287f6b7f785
BrianASC23 <bcfrags@gmail.com>
2023-08-03 17:33:04 -0400

    first commit

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,10 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+setInterval(function() {
+    time ++;
+    counterElement.innerHTML = time;
+}, 1000);
+
+
+
